441cf9cae6e552e0851e9bfb1114a429c1720aca
Vlad Isaienko <kisaievla@icloud.com>
2024-07-19 18:37:53 +0200

    Norming things

--- so_long.c
+++ so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: visaienk <visaienk@student.42prague.com>   +#+  +:+       +#+        */
+/*   By: visaienk <visaienk@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 12:14:14 by visaienk          #+#    #+#             */
-/*   Updated: 2024/07/19 17:45:36 by visaienk         ###   ########.fr       */
+/*   Updated: 2024/07/19 18:36:02 by visaienk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ void	load_textures(void *param)
 	map = param;
 	map->mlx_assets->sprite_t = mlx_load_xpm42("./textures/sprite.xpm42");
 	if (!map->mlx_assets->sprite_t)
-        	ft_printf("sprite didn't load");
+		ft_printf("sprite didn't load");
 	map->mlx_assets->floor_t = mlx_load_xpm42("./textures/floor.xpm42");
 	if (!map->mlx_assets->floor_t)
-        	ft_printf("floor didn't load");
+		ft_printf("floor didn't load");
 	map->mlx_assets->wall_t = mlx_load_xpm42("./textures/wall.xpm42");
 	if (!map->mlx_assets->wall_t)
-        	ft_printf("wall didn't load");
+		ft_printf("wall didn't load");
 	map->mlx_assets->coll_t = mlx_load_xpm42("./textures/collect.xpm42");
 	if (!map->mlx_assets->coll_t)
-        	ft_printf("sprite didn't load");
+		ft_printf("sprite didn't load");
 	map->mlx_assets->exit_t = mlx_load_xpm42("./textures/exit.xpm42");
 	if (!map->mlx_assets->exit_t)
-        	ft_printf("sprite didn't load");
+		ft_printf("sprite didn't load");
 }
 
-void make_image(void *param)
+void	make_image(void *param)
 {
 	t_map	*map;
 	mlx_t	*mlx;
@@ -42,68 +42,66 @@ void make_image(void *param)
 	map = param;
 	mlx = map->mlx;
 	map->mlx_assets->sprite = mlx_texture_to_image(mlx,
-		&map->mlx_assets->sprite_t->texture);
+			&map->mlx_assets->sprite_t->texture);
 	if (!map->mlx_assets->sprite)
-        	ft_printf("sprite error");
+		ft_printf("sprite error");
 	map->mlx_assets->floor = mlx_texture_to_image(mlx,
-		&map->mlx_assets->floor_t->texture);
+			&map->mlx_assets->floor_t->texture);
 	if (!map->mlx_assets->floor)
-        	ft_printf("texture error");
+		ft_printf("texture error");
 	map->mlx_assets->wall = mlx_texture_to_image(mlx,
-		&map->mlx_assets->wall_t->texture);
+			&map->mlx_assets->wall_t->texture);
 	if (!map->mlx_assets->wall)
-        	ft_printf("texture error");
+		ft_printf("texture error");
 	map->mlx_assets->coll = mlx_texture_to_image(mlx,
-		&map->mlx_assets->coll_t->texture);
+			&map->mlx_assets->coll_t->texture);
 	if (!map->mlx_assets->coll)
-        	ft_printf("texture error");
+		ft_printf("texture error");
 	map->mlx_assets->exit = mlx_texture_to_image(mlx,
-		&map->mlx_assets->exit_t->texture);
+			&map->mlx_assets->exit_t->texture);
 	if (!map->mlx_assets->exit)
-        	ft_printf("texture error");
+		ft_printf("texture error");
 }
-
 
 void	put_img(void *param)
 {
 	t_map	*map;
-	mlx_t	*mlx;
-	int	i;
-	int	j;
-	int	x;
-	int	y;
+	int		i;
+	int		j;
+	int		x;
+	int		y;
 
 	map = param;
-	mlx = map->mlx;
 	i = 0;
-	x = 0;	
+	x = 0;
 	while (i < map->height)
 	{	
 		j = 0;
 		y = 0;
 		while (j < map->width)
 		{
-			if (i < map->height && j < map->width && map->data[i][j] == '1')
+			if (i < map->height && j < map->width
+				&& map->data[i][j] == '1')
 			{
-				if (mlx_image_to_window(mlx, map->mlx_assets->wall, y, x) < 0)
-        				ft_printf("wall error");
+				if (mlx_image_to_window(map->mlx,
+						map->mlx_assets->wall, y, x) < 0)
+					ft_printf("wall error");
 			}
-			else if (i < map->height && j < map->width && map->data[i][j] == '0')
+			else if (i < map->height && j < map->width
+				&& map->data[i][j] == 'C')
 			{
-				if (mlx_image_to_window(mlx, map->mlx_assets->floor, y, x) < 0)
-        				ft_printf("floor error");
-			}
-			else if (i < map->height && j < map->width && map->data[i][j] == 'C')
-			{
-				if (mlx_image_to_window(mlx, map->mlx_assets->floor, y, x) < 0)
-        				ft_printf("floor error");
-				if (mlx_image_to_window(mlx, map->mlx_assets->coll, y, x) < 0)
-        				ft_printf("collect error");
+				if (mlx_image_to_window(map->mlx,
+						map->mlx_assets->floor, y, x) < 0)
+					ft_printf("floor error");
+				if (mlx_image_to_window(map->mlx,
+						map->mlx_assets->coll, y, x) < 0)
+					ft_printf("collect error");
 			}
 			else
 			{
-				if (mlx_image_to_window(mlx, map->mlx_assets->floor, y, x) < 0)
-        				ft_printf("floor error");	
+				if (mlx_image_to_window(map->mlx,
+						map->mlx_assets->floor, y, x) < 0)
+					ft_printf("floor error");
 			}
 			y += 50;
 			j++;
@@ -111,8 +109,9 @@ void	put_img(void *param)
 		x += 50;
 		i++;
 	}
-	if (mlx_image_to_window(mlx, map->mlx_assets->sprite, map->START_C * 50, map->START_R * 50) < 0)
-        	ft_printf("sprite error");
+	if (mlx_image_to_window(map->mlx, map->mlx_assets->sprite,
+			map->START_C * 50, map->START_R * 50) < 0)
+		ft_printf("sprite error");
 }
 
 void	my_keyhook(mlx_key_data_t keydata, void *param)
@@ -130,7 +129,7 @@ void	my_keyhook(mlx_key_data_t keydata, void *param)
         		ft_printf("exit error");
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE)
-		|| map->data[map->START_R][map->START_C] == 'E')
+		|| (map->data[map->START_R][map->START_C] == 'E' && map->COLLECTIBLE == 0))
 		mlx_close_window(mlx);
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
 		move_sprite_left(map, mlx, &map->steps);
